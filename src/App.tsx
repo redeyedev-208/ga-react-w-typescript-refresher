185bd822abe0cad2e42b02f3fb3abfd1b93ddc5a
gilaguilar <redeyedev.208@gmail.com>
2024-05-20 20:21:40 -0600

    using children first approach to consider

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,9 @@ import CourseGoal from './components/CourseGoal.tsx';
 export default function App() {
   return (
     <main>
-      <CourseGoal
-        title='React + TS Refresher'
-        description='Learn what has changed in Typescript'
-      />
+      <CourseGoal title='React + TS Refresher'>
+        <p>Refresher React w/ Typescript Fundamentals</p>
+      </CourseGoal>
     </main>
   );
 }
--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,17 +1,18 @@
+import { type ReactNode } from 'react';
 // Creating our custom type for our props
 // Not looking to extend this so we are ok for now with this custom type
 // This is how we can outsource out type
 type CourseGoalProps = {
   title: string;
-  description: string;
+  children: ReactNode;
 };
 
-export default function CourseGoal({ title, description }: CourseGoalProps) {
+export default function CourseGoal({ title, children }: CourseGoalProps) {
   return (
     <article>
       <div>
         <h2>{title}</h2>
-        <p>{description}</p>
+        <p>{children}</p>
         <button>Delete</button>
       </div>
     </article>
