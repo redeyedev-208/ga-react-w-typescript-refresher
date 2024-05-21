28bcf379c7b1f15f41c65201a2427da6975fccba
gilaguilar <redeyedev.208@gmail.com>
2024-05-20 20:08:53 -0600

    initial commit or react project with vite and tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import CourseGoal from './components/CourseGoal.tsx';
+
+export default function App() {
+  return (
+    <main>
+      <CourseGoal
+        title='React + TS Refresher'
+        description='Learn what has changed in Typescript'
+      />
+    </main>
+  );
+}
