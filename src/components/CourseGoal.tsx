0269fa3ae0a7d2965b7177ca2c1d1d7b09cf010b
gilaguilar <redeyedev.208@gmail.com>
2024-05-20 20:14:27 -0600

    created custom type for outsourcing and readability

--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,10 +1,12 @@
-export default function CourseGoal({
-  title,
-  description,
-}: {
+// Creating our custom type for our props
+// Not looking to extend this so we are ok for now with this custom type
+// This is how we can outsource out type
+type CourseGoalProps = {
   title: string;
   description: string;
-}) {
+};
+
+export default function CourseGoal({ title, description }: CourseGoalProps) {
   return (
     <article>
       <div>
