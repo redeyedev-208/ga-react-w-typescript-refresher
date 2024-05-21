caf73578c42fb6a68f71426f6983b6793097a8d0
gilaguilar <redeyedev.208@gmail.com>
2024-05-20 20:27:00 -0600

    using PropsWithChildren but leaving options commented out, will remove on the next push

--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,11 +1,21 @@
-import { type ReactNode } from 'react';
+import { type PropsWithChildren } from 'react';
 // Creating our custom type for our props
 // Not looking to extend this so we are ok for now with this custom type
 // This is how we can outsource out type
-type CourseGoalProps = {
-  title: string;
-  children: ReactNode;
-};
+// type CourseGoalProps = {
+//   title: string;
+//   children: ReactNode;
+// };
+
+// This is another approach aside from the Generic type used in the type that follows
+// interface CourseGoalProps {
+//   title: string;
+//   children: ReactNode
+// }
+
+// Main takeaway is to know how to get at the children when needed
+// This is a generic type that works with a specific props type
+type CourseGoalProps = PropsWithChildren<{ title: string }>;
 
 export default function CourseGoal({ title, children }: CourseGoalProps) {
   return (
